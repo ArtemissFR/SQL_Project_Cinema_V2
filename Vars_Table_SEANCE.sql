041d266f54f8ff56fba2a01d34541b7f89254817
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:09:35 +0100

    Create Vars_Table_SEANCE.sql

---
+++ Vars_Table_SEANCE.sql
@@ -0,0 +1,9 @@
+-- Générer des données factices pour la table SEANCE
+INSERT INTO SEANCE (salle_id, projection_id, film_id, reservation)
+VALUES
+    (1, 1, 1, 50),
+    (2, 2, 2, 80),
+    (3, 3, 3, 60),
+    (4, 4, 4, 40),
+    (5, 5, 5, 100);
+-- Ajoutez autant d'enregistrements que nécessaire
