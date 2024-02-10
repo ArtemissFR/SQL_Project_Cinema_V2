a30cd561754fa604c7596390356b03ecc1f8bede
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:46:52 +0100

    Update Full_Project.sql

--- Full_Project.sql
+++ Full_Project.sql
@@ -292,3 +292,17 @@ VALUES
 -- Ajoutez autant d'enregistrements que nécessaire
 
 ####################CRÉATION DE VALEURS FACTICES####################
+
+####################CRÉATION DE PROCEDURES####################
+
+DELIMITER //
+CREATE PROCEDURE GetFilmsByGenre(IN genre_name VARCHAR(30))
+BEGIN
+    SELECT film_titre, film_annee, genre_nom AS genre
+    FROM Films
+    JOIN Genres ON Films.genre_id = Genres.genre_id
+    WHERE genre_nom = genre_name;
+END //
+DELIMITER ;
+
+####################CRÉATION DE PROCEDURES####################
