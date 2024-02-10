d72d7e2c772e71411fbb3909ac3cccf58e936303
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:33:33 +0100

    Update Vars_Table_ALL.sql

--- VARS /Vars_Table_ALL.sql
+++ VARS /Vars_Table_ALL.sql
@@ -46,5 +46,17 @@ VALUES
     (5, 'Salle E', 200);
 -- Ajoutez autant d'enregistrements que nécessaire
 
-#--[TABLE-SALLES]--
+#--[TABLE-THEME]--
+
+-- Générer des données factices pour la table THEME
+INSERT INTO THEME (theme_id, theme_nom)
+VALUES
+    (1, 'Amour'),
+    (2, 'Aventure'),
+    (3, 'Fantaisie'),
+    (4, 'Mystère'),
+    (5, 'Historique');
+-- Ajoutez autant d'enregistrements que nécessaire
+
+#--[TABLE-FILMS]--
 
