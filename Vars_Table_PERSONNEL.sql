87da42a46ec7e63a471884ff96cfecd6b60446fe
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:05:54 +0100

    Create Vars_Table_PERSONNEL.sql

---
+++ Vars_Table_PERSONNEL.sql
@@ -0,0 +1,9 @@
+-- Générer des données factices pour la table PERSONNEL
+INSERT INTO PERSONNEL (realisateur_id, acteur_id, film_id, effectif)
+VALUES
+    (1, 2, 1, 10),
+    (3, 3, 2, 8),
+    (2, 4, 3, 15),
+    (4, 5, 4, 12),
+    (5, 1, 5, 9);
+-- Ajoutez autant d'enregistrements que nécessaire
