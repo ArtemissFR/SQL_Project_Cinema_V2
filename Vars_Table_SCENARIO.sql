2f9a21dcd4cf501a478608735adaf1f9e838d223
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:07:39 +0100

    Create Vars_Table_SCENARIO.sql

---
+++ Vars_Table_SCENARIO.sql
@@ -0,0 +1,9 @@
+-- Générer des données factices pour la table SCENARIO
+INSERT INTO SCENARIO (genre_id, theme_id, film_id)
+VALUES
+    (1, 1, 1),
+    (2, 2, 2),
+    (3, 3, 3),
+    (4, 4, 4),
+    (5, 5, 5);
+-- Ajoutez autant d'enregistrements que nécessaire
