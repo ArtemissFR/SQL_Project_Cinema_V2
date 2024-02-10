385d5d1229fffcb2f9792dcc482a31edde8cebd0
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:02:19 +0100

    Create Vars_Table_REALISATEUR.sql

---
+++ Vars_Table_REALISATEUR.sql
@@ -0,0 +1,9 @@
+-- Générer des données factices pour la table REALISATEUR
+INSERT INTO REALISATEUR (realisateur_id, realisateur_nom, realisateur_prenom, realisateur_date_naissance)
+VALUES
+    (1, 'Spielberg', 'Steven', '1946-12-18'),
+    (2, 'Tarantino', 'Quentin', '1963-03-27'),
+    (3, 'Nolan', 'Christopher', '1970-07-30'),
+    (4, 'Coppola', 'Francis Ford', '1939-04-07'),
+    (5, 'Scorsese', 'Martin', '1942-11-17');
+-- Ajoutez autant d'enregistrements que nécessaire
