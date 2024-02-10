46a0c9b96ac03250a8e201bdbeaa60cd932bd761
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:36:25 +0100

    Update Vars_Table_ALL.sql

--- VARS /Vars_Table_ALL.sql
+++ VARS /Vars_Table_ALL.sql
@@ -107,3 +107,13 @@ VALUES
 -- Ajoutez autant d'enregistrements que nécessaire
 
 #--[TABLE-SEANCE]--
+
+-- Générer des données factices pour la table SEANCE
+INSERT INTO SEANCE (salle_id, projection_id, film_id, reservation)
+VALUES
+    (1, 1, 1, 50),
+    (2, 2, 2, 80),
+    (3, 3, 3, 60),
+    (4, 4, 4, 40),
+    (5, 5, 5, 100);
+-- Ajoutez autant d'enregistrements que nécessaire
