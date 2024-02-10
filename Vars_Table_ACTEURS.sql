e21f062a0977434c55c80e03bed70dcd05f5584f
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 16:56:48 +0100

    Create Vars_Table_ACTEURS.sql

---
+++ Vars_Table_ACTEURS.sql
@@ -0,0 +1,9 @@
+-- Générer des données factices pour la table ACTEURS
+INSERT INTO ACTEURS (acteur_id, acteur_nom, acteur_prenom, acteur_date_naissance)
+VALUES
+    (1, 'Holland', 'Tom', '1996-06-01'),
+    (2, 'Smith', 'Will', '1968-09-25'),
+    (3, 'Pitt', 'Brad', '1963-12-18'),
+    (4, 'Blunt', 'Emily', '1983-02-23'),
+    (5, 'Phoenix', 'Joaquin', '1974-10-28');
+-- Ajoutez autant d'enregistrements que nécessaire
