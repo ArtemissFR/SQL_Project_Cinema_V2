fe99b58156dba4f985180da9dbf9c5fde1add19f
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:33:53 +0100

    Update Vars_Table_ALL.sql

--- VARS /Vars_Table_ALL.sql
+++ VARS /Vars_Table_ALL.sql
@@ -60,3 +60,14 @@ VALUES
 
 #--[TABLE-FILMS]--
 
+-- Générer des données factices pour la table FILMS
+INSERT INTO FILMS (film_id, film_titre, film_annee, genre_id, realisateur_id, acteur_id, theme_id)
+VALUES
+    (1, 'Le Voyage Fantastique', 2022, 3, 3, 1, 2),
+    (2, 'LÉnigme du Sphinx', 2020, 4, 2, 3, 4),
+    (3, 'Les Ombres du Passé', 2018, 1, 5, 4, 1),
+    (4, 'LÉclipse Lunaire', 2019, 2, 1, 2, 3),
+    (5, 'LÉvasion Inattendue', 2021, 5, 4, 5, 5);
+-- Ajoutez autant d'enregistrements que nécessaire
+
+#--[TABLE-FILMS]--
