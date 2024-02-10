edd37b9a55a36af5ecbaa584a6c87489d4278bfa
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:32:58 +0100

    Update Vars_Table_ALL.sql

--- VARS /Vars_Table_ALL.sql
+++ VARS /Vars_Table_ALL.sql
@@ -34,5 +34,17 @@ VALUES
     (5, 'Scorsese', 'Martin', '1942-11-17');
 -- Ajoutez autant d'enregistrements que nécessaire
 
-#--[TABLE-REALISATEUR]--
+#--[TABLE-SALLES]--
+
+-- Générer des données factices pour la table SALLES
+INSERT INTO SALLES (salle_id, salle_nom, salle_capacite)
+VALUES
+    (1, 'Salle A', 100),
+    (2, 'Salle B', 150),
+    (3, 'Salle C', 120),
+    (4, 'Salle D', 80),
+    (5, 'Salle E', 200);
+-- Ajoutez autant d'enregistrements que nécessaire
+
+#--[TABLE-SALLES]--
 
