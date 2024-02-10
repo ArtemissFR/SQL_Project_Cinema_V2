5a4138bbb6e45aa6a6888aff66fa1b2c8ec0959e
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:34:29 +0100

    Update Vars_Table_ALL.sql

--- VARS /Vars_Table_ALL.sql
+++ VARS /Vars_Table_ALL.sql
@@ -70,4 +70,16 @@ VALUES
     (5, 'LÉvasion Inattendue', 2021, 5, 4, 5, 5);
 -- Ajoutez autant d'enregistrements que nécessaire
 
-#--[TABLE-FILMS]--
+#--[TABLE-PROJECTIONS]--
+
+-- Générer des données factices pour la table PROJECTIONS
+INSERT INTO PROJECTIONS (projection_id, film_id, salle_id, projection_date_heure)
+VALUES
+    (1, 1, 1, '2024-02-10 15:00:00'),
+    (2, 2, 2, '2024-02-11 18:30:00'),
+    (3, 3, 3, '2024-02-12 21:15:00'),
+    (4, 4, 4, '2024-02-13 14:45:00'),
+    (5, 5, 5, '2024-02-14 17:30:00');
+-- Ajoutez autant d'enregistrements que nécessaire
+
+#--[TABLE-PROJECTIONS]--
