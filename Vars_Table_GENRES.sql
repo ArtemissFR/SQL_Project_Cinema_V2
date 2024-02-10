77678b9827d235cf9c8f47a0a3241d4e02acab96
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 16:58:48 +0100

    Create Vars_Table_GENRES.sql

---
+++ Vars_Table_GENRES.sql
@@ -0,0 +1,9 @@
+-- Générer des données factices pour la table GENRES
+INSERT INTO GENRES (genre_id, genre_nom)
+VALUES
+    (1, 'Action'),
+    (2, 'Comédie'),
+    (3, 'Drame'),
+    (4, 'Science-Fiction'),
+    (5, 'Thriller');
+-- Ajoutez autant d'enregistrements que nécessaire
