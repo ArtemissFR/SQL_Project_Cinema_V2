73cd75f59ed1da6ea9f6583070505e6965b1e3bf
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-10 17:47:27 +0100

    Update Full_Project.sql

--- Full_Project.sql
+++ Full_Project.sql
@@ -306,3 +306,13 @@ END //
 DELIMITER ;
 
 ####################CRÉATION DE PROCEDURES####################
+
+####################CRÉATION DE VUES####################
+
+CREATE VIEW FullProjectionDetails AS
+SELECT Projections.projection_id, Films.film_titre, Salles.salle_nom AS salle, Projections.projection_date_heure
+FROM Projections
+JOIN Films ON Projections.film_id = Films.film_id
+JOIN Salles ON Projections.salle_id = Salles.salle_id;
+
+####################CRÉATION DE VUES####################
