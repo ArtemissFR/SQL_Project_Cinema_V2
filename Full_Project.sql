fc6622d83594ee7546afa64eb46e5db6b30f9f65
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-16 09:00:43 +0100

    Update Full_Project.sql

--- Full_Project.sql
+++ Full_Project.sql
@@ -329,3 +329,20 @@ SET total_projections = total_projections + 1
 WHERE film_id = NEW.film_id;
 
 ####################CRÉATION DE TRIGGERS####################
+
+####################CRÉATION D EVENEMENT###################
+	
+-- Création de l'événement pour le Festival du Cinéma Multigenre
+DELIMITER //
+CREATE EVENT IF NOT EXISTS festival_cinema_multigenre
+ON SCHEDULE AT '2024-07-10 18:00:00' -- Date et heure du début de l'événement
+DO
+BEGIN
+    -- Insertion d'une projection spéciale du film avec l'ID 1
+    INSERT INTO PROJECTIONS (film_id, salle_id, projection_date_heure)
+    VALUES (1, 1, '2024-07-10 18:00:00'); -- Vous devez ajuster la salle_id et la date et l'heure selon votre base de données.
+END;
+//
+DELIMITER ;
+
+####################CRÉATION D EVENEMENT###################
