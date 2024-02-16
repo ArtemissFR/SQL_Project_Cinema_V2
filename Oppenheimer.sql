9b4e9a860ad891dbd19f0c723df45c618108f43e
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-02-16 09:19:14 +0100

    Update Oppenheimer.sql

--- Oppenheimer.sql
+++ Oppenheimer.sql
@@ -1,6 +1,6 @@
 INSERT INTO ACTEURS (acteur_id, acteur_nom, acteur_prenom, acteur_date_naissance) -- Ajout de l'acteur non présent
 VALUES
-    (6, 'Murphy', 'Cillian', '1976-05-25'),
+    (6, 'Murphy', 'Cillian', '1976-05-25');
 
 INSERT INTO FILMS (film_id, film_titre, film_annee, genre_id, realisateur_id, acteur_id, theme_id) -- Ajout du film
 VALUES
