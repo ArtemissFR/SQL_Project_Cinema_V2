dbf555e9f39130107be2583300bbbeb36b8302be
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-07 15:35:14 +0100

    Delete VARS /Vars_Table_SALLES.sql

--- VARS /Vars_Table_SALLES.sql
+++
@@ -1,9 +0,0 @@
--- Générer des données factices pour la table SALLES
-INSERT INTO SALLES (salle_id, salle_nom, salle_capacite)
-VALUES
-    (1, 'Salle A', 100),
-    (2, 'Salle B', 150),
-    (3, 'Salle C', 120),
-    (4, 'Salle D', 80),
-    (5, 'Salle E', 200);
--- Ajoutez autant d'enregistrements que nécessaire
