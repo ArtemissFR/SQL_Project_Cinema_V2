bfb9cf2e262fd4dd088abdcc5b2deb743e15348e
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-07 15:35:26 +0100

    Delete VARS /Vars_Table_THEME.sql

--- VARS /Vars_Table_THEME.sql
+++
@@ -1,9 +0,0 @@
--- Générer des données factices pour la table THEME
-INSERT INTO THEME (theme_id, theme_nom)
-VALUES
-    (1, 'Amour'),
-    (2, 'Aventure'),
-    (3, 'Fantaisie'),
-    (4, 'Mystère'),
-    (5, 'Historique');
--- Ajoutez autant d'enregistrements que nécessaire
