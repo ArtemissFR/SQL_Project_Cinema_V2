3f4c3e6862dabc02f0b381141e50335385db12b1
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-07 15:53:54 +0100

    Update GetInformationFilm.sql

--- REQUEST/GetInformationFilm.sql
+++ REQUEST/GetInformationFilm.sql
@@ -5,8 +5,6 @@ SELECT
     REALISATEUR.realisateur_nom,
     REALISATEUR.realisateur_prenom,
     GROUP_CONCAT(DISTINCT ACTEURS.acteur_nom ORDER BY ACTEURS.acteur_nom ASC SEPARATOR ', ') AS acteurs,
-    GENRES.genre_nom,
-    THEME.theme_nom,
     PROJECTIONS.salle_id,
     SALLES.salle_nom,
     PROJECTIONS.projection_date_heure
@@ -16,8 +14,6 @@ JOIN REALISATEUR ON FILMS.realisateur_id = REALISATEUR.realisateur_id
 JOIN PERSONNEL ON FILMS.film_id = PERSONNEL.film_id
 JOIN ACTEURS ON PERSONNEL.acteur_id = ACTEURS.acteur_id
 JOIN SCENARIO ON FILMS.film_id = SCENARIO.film_id
-JOIN GENRES ON SCENARIO.genre_id = GENRES.genre_id
-JOIN THEME ON SCENARIO.theme_id = THEME.theme_id
 JOIN PROJECTIONS ON FILMS.film_id = PROJECTIONS.film_id
 JOIN SALLES ON PROJECTIONS.salle_id = SALLES.salle_id
 WHERE
@@ -25,5 +21,4 @@ WHERE
 GROUP BY
     FILMS.film_id, FILMS.film_titre, FILMS.film_annee,
     REALISATEUR.realisateur_nom, REALISATEUR.realisateur_prenom,
-    GENRES.genre_nom, THEME.theme_nom,
     PROJECTIONS.salle_id, SALLES.salle_nom, PROJECTIONS.projection_date_heure;
